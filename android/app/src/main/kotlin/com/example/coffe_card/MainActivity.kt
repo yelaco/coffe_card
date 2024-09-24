20d4937657b2f9707169e5a44393050e2bb2d894
Minh Quang Bui <minhquangbui053@gmail.com>
2024-09-24 17:52:55 +0700

    Init

---
+++ android/app/src/main/kotlin/com/example/coffe_card/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coffe_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('My Coffee Id'),
+      ),
+    ),
+  ));
+}
